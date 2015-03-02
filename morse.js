a1e3b3f792979c4acdab59c8efbca7087d3c952e
Subhash A <s.a@sap.com>
2015-03-02 19:51:59 +0530

    added styles and better parsing

--- morse.js
+++ morse.js
@@ -4,15 +4,22 @@ var MorseMachine = function() {
 	Object.keys(codemap).map(function(k){reversemap[codemap[k]] = k});
 	return {
 	    "encode": function (string) {
-		return string.split("").map(function(c){ return codemap[c.toUpperCase()] }).join("|");
+		return string
+		    .split("")
+		    .map(function(c){ return codemap[c.toUpperCase()] || c})
+		    .join(" ");
 	    },
 	    "decode": function (data) {
-		return data.split("|").map(function(m){ return reversemap[m.trim()] }).join(" ");
+		data = data.replace(/\s{7}/gi," | ");
+		return data
+		    .split(" ")
+		    .map(function(m){ return reversemap[m.trim()] || m })
+		    .join("")
+		    .replace(/\|/gi," ");
 	    }
 	}
 };
     
-
 var m = new MorseMachine();
 
 var encodeString = function () {
